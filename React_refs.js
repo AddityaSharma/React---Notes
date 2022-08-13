ff87ed8912d2f36ecb0cfc452f9de97e0e845e04
Adi_Anant <72218536+AddityaSharma@users.noreply.github.com>
2022-08-13 21:48:34 +0530

    Update React_refs.js

--- React_refs.js
+++ React_refs.js
@@ -1,3 +1,6 @@
+/* Refs make it possible to access DOM nodes directly within the structure in react. */
+/* Refs are only available within a class component */
+
 /* Method-1: (preffered) */
 import React, { Component } from 'react'
 
@@ -52,3 +55,91 @@ export class RefsDemo extends Component {
 }
 
 export default RefsDemo
+
+/* ref to a child component from a parent component: */
+// Parent Component:
+import React, { Component } from 'react'
+import Input from './Input';
+
+class FocusInput extends Component {
+  constructor(props) {
+    super(props)
+    this.componentRef = React.createRef()
+  }
+
+  clickHandler = () => {
+    this.componentRef.current.focusInput()
+  }
+
+  render() {
+    return (
+      <div>
+        <Input ref={this.componentRef}></Input>
+        <button onClick={this.clickHandler}>Focus Input</button>
+      </div>
+    )
+  }
+}
+
+export default FocusInput
+
+// Child Component:
+import React, { Component } from 'react'
+
+class Input extends Component {
+  constructor(props) {
+    super(props)
+    this.inputRef = React.createRef()
+  }
+
+  focusInput() {
+    this.inputRef.current.focus()
+  }
+
+  render() {
+    return (
+      <input type="text" ref={this.inputRef}></input>
+    )
+  }
+}
+
+export default Input
+
+/* Ref Fowarding: */
+// Parent Component:
+import React, { Component } from 'react'
+import FRInput from './FRInput'
+
+export class FRParentInput extends Component {
+  constructor(props) {
+    super(props)
+    this.inputRef = React.createRef()
+  }
+
+  clickHandler = () => {
+    this.inputRef.current.focus()
+  }
+
+  render() {
+	return (
+	 		<div>
+        <FRInput ref={this.inputRef} />
+        <button onClick={this.clickHandler}>Focus Input</button>
+	 		</div>
+    )}
+}
+
+export default FRParentInput
+
+// Child Component:
+import React from 'react'
+
+const FRInput = React.forwardRef((props, ref) => {
+	return (
+		<div>
+      <input type="text" ref={ref} />
+		</div>
+	)
+})
+
+export default FRInput
